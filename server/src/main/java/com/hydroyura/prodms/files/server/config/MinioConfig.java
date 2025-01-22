89a423f74c073d955bcfe8d08e318df2fa1c9195
yury.klimchuk <hydro.yura@gmail.com>
2025-01-22 22:42:03 +0300

    update

--- server/src/main/java/com/hydroyura/prodms/files/server/config/MinioConfig.java
+++ server/src/main/java/com/hydroyura/prodms/files/server/config/MinioConfig.java
@@ -6,12 +6,16 @@ import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 import io.minio.UploadObjectArgs;
+import io.minio.messages.Tags;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.KeyStore;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.util.Map;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
@@ -27,7 +31,6 @@ public class MinioConfig {
     @Bean
     OkHttpClient okHttpClient(MinioProps props) {
         var httpClientBuilder = new OkHttpClient.Builder();
-        insertSslContext(httpClientBuilder, props);
         return httpClientBuilder.build();
     }
 
@@ -35,7 +38,7 @@ public class MinioConfig {
     MinioClient minioClient(MinioProps props, OkHttpClient httpClient) {
         return MinioClient.builder()
             .endpoint(props.getUrl())
-            //.httpClient(httpClient)
+            .httpClient(httpClient)
             .credentials(props.getAccessKey(), props.getAccessSecret())
             .build();
     }
@@ -43,52 +46,62 @@ public class MinioConfig {
     @Bean
     String test(MinioClient minioClient) throws Exception {
 
-        minioClient.makeBucket(
-            MakeBucketArgs
-                .builder()
-                .bucket("user1")
-                .build());
+//        minioClient.makeBucket(
+//            MakeBucketArgs
+//                .builder()
+//                .bucket("user1")
+//                .build());
 
-        try (InputStream stream =
-                 minioClient.getObject(GetObjectArgs
-                     .builder()
-                     .bucket("user1")
-                     .object("example.pdf")
-                     .build())) {
-            // Read the stream
-        }
+//        try (InputStream stream =
+//                 minioClient.getObject(GetObjectArgs
+//                     .builder()
+//                     .bucket("user1")
+//                     .object("example.pdf")
+//                     .build())) {
+//            // Read the stream
+//        }
 
-        minioClient.uploadObject(
-            UploadObjectArgs.builder()
-                .bucket("user1")
-                .object("application.yaml")
-                .filename("/home/klimchuk/IdeaProjects/prodms/prodms-files/server/target/classes/application.yaml")
-                .build()
-        );
+//        minioClient.uploadObject(
+//            UploadObjectArgs.builder()
+//                .bucket("user1")
+//                .object("application.yaml")
+//                .filename("/home/klimchuk/IdeaProjects/prodms/prodms-files/server/target/classes/application.yaml")
+//                .build()
+//        );
 
         new ClassPathResource("application.yaml").getInputStream();
+
+        putObject(minioClient, "example_3.pdf");
 
         return "";
     }
 
-
-    private void insertSslContext(OkHttpClient.Builder builder, MinioProps props) {
-        try (var is = new ByteArrayInputStream(props.getCert().getBytes(StandardCharsets.UTF_8))) {
-            CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
-            X509Certificate cert = (X509Certificate) certFactory.generateCertificate(is);
-            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            keyStore.load(null, null);
-            keyStore.setCertificateEntry("MINIO_CACERT", cert);
-            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-            tmf.init(keyStore);
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, tmf.getTrustManagers(), null);
-            builder.sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) tmf.getTrustManagers()[0]);
-        } catch (Exception e) {
-            // TODO: create custom ex
-            throw new RuntimeException("");
-        }
+    private InputStream file(String name) throws Exception {
+        return new ClassPathResource(name).getInputStream();
     }
 
+    private void putObject(MinioClient client, String file) throws Exception {
+        var headers = Map.of(
+            "key1", "val1",
+            "key2", "val2",
+            "key3", "val3",
+            "key4", "val4"
+        );
+        var is = new ByteArrayInputStream(
+            Files.readAllBytes(
+                Paths.get(this.getClass().getClassLoader().getResource(file).toURI()))
+        );
+        var result = client.putObject(
+            PutObjectArgs.builder()
+                .bucket("user1")
+                .object(file)
+                .tags(Tags.newObjectTags(Map.of("tag1","vTag1", "tag2", "vTag2")))
+                .userMetadata(headers)
+                .stream(is, is.available(), -1)
+                .build()
+        );
+        is.close();
+        int a = 1;
+    }
 
 }
