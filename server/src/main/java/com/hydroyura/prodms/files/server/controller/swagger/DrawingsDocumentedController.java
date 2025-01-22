509c2f3cdec2b5c5d75a9bae82eb50feb5f7e382
YKlimchuk_li <yklimchuk_li@inno.tech>
2025-01-22 16:33:36 +0300

    added api docs

---
+++ server/src/main/java/com/hydroyura/prodms/files/server/controller/swagger/DrawingsDocumentedController.java
@@ -0,0 +1,22 @@
+package com.hydroyura.prodms.files.server.controller.swagger;
+
+
+import com.hydroyura.prodms.files.server.api.ApiRes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public interface DrawingsDocumentedController {
+
+
+    @RequestMapping(method = RequestMethod.GET, value = "api/v1/drawings/{number}")
+    ResponseEntity<ApiRes<?>> getLatest(String number);
+
+    @RequestMapping(method = RequestMethod.GET, value = "api/v1/drawings/{number}/all")
+    ResponseEntity<ApiRes<?>> getAll(String number);
+
+    @RequestMapping(method = RequestMethod.POST, value = "api/v1/drawings/{number}")
+    ResponseEntity<ApiRes<?>> add(String number);
+
+
+}
