4d9f6051108f1c1921a3bc11065376a2232cfe0d
YKlimchuk_li <yklimchuk_li@inno.tech>
2025-01-24 11:36:50 +0300

    added two initial tests

--- server/src/main/java/com/hydroyura/prodms/files/server/data/DrawingsRepository.java
+++ server/src/main/java/com/hydroyura/prodms/files/server/data/DrawingsRepository.java
@@ -2,14 +2,18 @@ package com.hydroyura.prodms.files.server.data;
 
 import io.minio.GetPresignedObjectUrlArgs;
 import io.minio.MinioClient;
+import io.minio.StatObjectArgs;
+import io.minio.StatObjectResponse;
+import io.minio.errors.ErrorResponseException;
 import io.minio.http.Method;
 import java.util.Optional;
-import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class DrawingsRepository {
 
     private final MinioClient minioClient;
@@ -23,16 +27,39 @@ public class DrawingsRepository {
 
 
     @SneakyThrows
-    public Optional<String> getDrawingUrl(String number) {
-        return Optional.ofNullable(
-            minioClient.getPresignedObjectUrl(
-                GetPresignedObjectUrlArgs.builder()
-                    .bucket(bucket)
-                    .method(Method.GET)
-                    .object(number)
-                    .build()
-            )
+    private String getDrawingUrlInternal(String object) {
+        return minioClient.getPresignedObjectUrl(
+            GetPresignedObjectUrlArgs.builder()
+                .bucket(bucket)
+                .method(Method.GET)
+                .object(object)
+                .build()
         );
+    }
+
+    public Optional<String> getDrawingUrl(String object) {
+        return getMetadata(object)
+            .map(StatObjectResponse::object)
+            .map(this::getDrawingUrlInternal);
+    }
+
+    //@SneakyThrows
+    public Optional<StatObjectResponse> getMetadata(String object) {
+        try {
+            return Optional.ofNullable(
+                minioClient.statObject(
+                    StatObjectArgs.builder()
+                        .bucket(bucket)
+                        .object(object)
+                        .build()
+            ));
+        } catch (ErrorResponseException e) {
+            // TODO: make pretty ex handling
+            log.warn("", e);
+            return Optional.empty();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
--- server/src/test/java/com/hydroyura/prodms/files/server/data/DrawingsRepositoryTest.java
+++ server/src/test/java/com/hydroyura/prodms/files/server/data/DrawingsRepositoryTest.java
@@ -1,20 +1,23 @@
 package com.hydroyura.prodms.files.server.data;
 
+import static com.hydroyura.prodms.files.server.data.DrawingRepositoryTestUnits.MINIO_CMD_CREATE_FILE;
+import static com.hydroyura.prodms.files.server.data.DrawingRepositoryTestUnits.MINIO_CMD_PLACE_FILE_TO_BUCKET;
 import static com.hydroyura.prodms.files.server.data.DrawingRepositoryTestUnits.MINIO_DOCKER_IMAGE;
 import static com.hydroyura.prodms.files.server.data.DrawingRepositoryTestUnits.MINIO_DRAWING_BUCKET;
 import static com.hydroyura.prodms.files.server.data.DrawingRepositoryTestUnits.MINIO_PWD;
 import static com.hydroyura.prodms.files.server.data.DrawingRepositoryTestUnits.MINIO_TEST_CONTAINER_NAME;
+import static com.hydroyura.prodms.files.server.data.DrawingRepositoryTestUnits.MINIO_TEST_FILE_CONTENT_1;
+import static com.hydroyura.prodms.files.server.data.DrawingRepositoryTestUnits.MINIO_TEST_FILE_NAME_1;
 import static com.hydroyura.prodms.files.server.data.DrawingRepositoryTestUnits.MINIO_USER;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.minio.MinioClient;
-import java.util.List;
 import org.junit.ClassRule;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.MinIOContainer;
-import org.testcontainers.utility.DockerImageName;
 
 
 class DrawingsRepositoryTest {
@@ -31,12 +34,22 @@ class DrawingsRepositoryTest {
     @BeforeAll
     static void startContainer() throws Exception {
         TEST_CONTAINER.start();
-        TEST_CONTAINER.execInContainer("bash", "-c", "mc mb data/%s".formatted(MINIO_DRAWING_BUCKET));
+        TEST_CONTAINER.execInContainer("bash", "-c",
+            "mc config host add minio-test http://localhost:9000 %s %s".formatted(MINIO_USER, MINIO_PWD));
+        TEST_CONTAINER.execInContainer("bash", "-c",
+            "mc mb --with-versioning minio-test/%s".formatted(MINIO_DRAWING_BUCKET));
     }
 
     @AfterAll
     static void closeContainer() {
         TEST_CONTAINER.close();
+    }
+
+
+    @AfterEach
+    void clearBucket() throws Exception {
+        TEST_CONTAINER.execInContainer("bash", "-c",
+            "mc rm --recursive --force minio-test/%s/".formatted(MINIO_DRAWING_BUCKET));
     }
 
     DrawingsRepositoryTest() {
@@ -48,35 +61,30 @@ class DrawingsRepositoryTest {
     }
 
     @Test
-    void test() {
-        int a = 1;
+    void getDrawingUrl_FOR_NOT_EXISTED_FILE() {
+        var result = repository.getDrawingUrl(MINIO_TEST_FILE_NAME_1);
+        assertTrue(result.isEmpty());
     }
 
+    @Test
+    void getDrawingUrl_FOR_EXISTED_FILE() throws Exception {
+        // given
+        TEST_CONTAINER.execInContainer(
+            "bash",
+            "-c",
+            MINIO_CMD_CREATE_FILE.formatted(MINIO_TEST_FILE_NAME_1, MINIO_TEST_FILE_CONTENT_1)
+        );
+        TEST_CONTAINER.execInContainer(
+            "bash",
+            "-c",
+            MINIO_CMD_PLACE_FILE_TO_BUCKET.formatted(MINIO_TEST_FILE_NAME_1, MINIO_DRAWING_BUCKET, MINIO_TEST_FILE_NAME_1)
+        );
 
-    /*
-    cat <<'EOF' > example.txt
-Это строка 1
-Это строка 2
-Это строка 3
-EOF
-     */
+        // when
+        var result = repository.getDrawingUrl(MINIO_TEST_FILE_NAME_1);
 
-
-
-    /*
-
-
-# Создаем переменную с текстом
-TEXT="Это строка 1
-Это строка 2
-Это строка 3"
-
-# Записываем значение переменной в файл
-echo "$TEXT" > example.txt
-
-
-     */
-
-
+        // then
+        assertTrue(result.isPresent());
+    }
 
 }
